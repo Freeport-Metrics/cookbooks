fe2990a848e3abd86124ba303463dd522ded901a
Filip Tepper <filip@tepper.pl>
2010-12-20 15:50:40 +0100

    bump version according what's on the chef-server

--- application/metadata.rb
+++ application/metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Freeport Metrics"
 maintainer_email "info@freeportmetrics.com"
 license          "MIT"
 description      "Configures nginx/Passenger stack for Capistrano deployments"
-version          "0.0.2"
+version          "1.0.1"
 depends          "passenger"
 
 recipe           "application::passenger", "Configures Rack-based application for nginx/Passenger stack"
